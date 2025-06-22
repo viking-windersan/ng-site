82e3ae679d31cb15233ae4ef9b026e313f99add2
viking-windersan <vikingsoftworks@outlook.com>
2025-06-22 15:31:40 -0700

    mat card

--- vikingr/src/app/app.component.ts
+++ vikingr/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'vikingr';
 }
+
+
